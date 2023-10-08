1d9a6e466d0f2ba14696ec5b4c345280354a70d1
Javid Shaikh <55527172+javidshaikhf@users.noreply.github.com>
2023-10-08 13:08:06 +0530

    Update TabBar.swift

--- TabBar.swift
+++ TabBar.swift
@@ -1,9 +1,13 @@
 //
-//  Home.swift
-//  BottomTabBar
+// Created By: Mobile Apps Academy
+// Subscribe : https://www.youtube.com/@MobileAppsAcademy?sub_confirmation=1
+// Medium Blob : https://medium.com/@mobileappsacademy
+// LinkedIn : https://www.linkedin.com/company/mobile-apps-academy
+// Twitter : https://twitter.com/MobileAppsAcdmy
+// Lisence : https://github.com/Mobile-Apps-Academy/MobileAppsAcademyLicense/blob/main/LICENSE.txt
 //
-//  Created by Javid Shaikh on 07/10/23.
-//
+
+
 
 import SwiftUI
 
